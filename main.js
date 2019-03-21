6301c92d28ebc5b6f4f34e942144fc765d8cc013
JPantana <joshpantana@me.com>
2019-03-20 20:43:03 -0500

    wip

--- main.js
+++ main.js
@@ -1,4 +1,7 @@
 const students = [];
+const houses = ['Gryffindor', 'Hufflepuff', 'Ravenclaw', 'Slytherin'];
+const exspelled = [];
+
 
 const printToDom = (divId, textToPrint) => {
     let selectedDiv = document.getElementById(divId);
@@ -8,17 +11,41 @@ const printToDom = (divId, textToPrint) => {
 
 const domStringBuilder = () => {
     let domString = ``;
-    students.forEach((student) => {
-        domString += ``;
-    });
-    printToDom(divId, domString);
+    // let students = [];
+    students = document.getElementById('hoggyName').value;
+    // students.forEach((student) => {
+    domString += `<p>${student}</p>`;
+        // domString += `<p>(math.random</p>`;
+    // });
+    printToDom('studentCards', domString);
 };
 
+
+const domFormBuilder = () => {
+    let domString = ``;
+        domString += `<form>`;
+        domString += `  <div class="form-group">`;
+        domString += `     <label for="hoggyName">Full Name</label>`;
+        domString += `     <input type="text" class="form-control" id="hoggyName" aria-describedby="emailHelp" placeholder="Enter full name">`;
+        domString += `  </div>`;
+        domString += `      <button type="submit" id="sortMe" class="btn btn-primary">Let's Get Sorted!</button>`;
+        domString += `</form>`;
+    
+
+    printToDom('sortingForm', domString);
+    document.getElementById('sortMe').addEventListener('click', domStringBuilder);
+};
+
+
+const hoggyForm = () => {
+
+}
+
 const buttonEvents = () => {
-    document.getElementById('').addEventListener('click', function())
+    document.getElementById('letsGetSorting').addEventListener('click', domFormBuilder);
 };
 
 const init = () => {
-    
+    buttonEvents();
 };
 init();
