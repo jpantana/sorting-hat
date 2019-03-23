d3eacead5e0d5d1798af5dae524dc6ba5d7f3a04
JPantana <joshpantana@me.com>
2019-03-23 14:04:06 -0500

    got the expel function to work... finally

--- main.js
+++ main.js
@@ -4,17 +4,21 @@ const exspelled = [];
 
 
 const printToDom = (divId, textToPrint) => {
+    console.log('print to dom is running');
+    console.log(textToPrint);
     let selectedDiv = document.getElementById(divId);
     selectedDiv.innerHTML = textToPrint;
 };
 
 
 const domStringBuilder = () => {
+    console.log('domStringBuilder is running', students);
         let domString = ``;
         students.forEach((student) => { // House Sorter 
+            console.log(student);
         domString += `<div class="card col-4 m">`;
         domString += `<h2 class="card-title">${student.name}</h2>`;
-        domString += `<h5 id="sortedHouse" class="card-text">${student.house}</h5>`;
+        domString += `<h5 class="card-text">${student.house}</h5>`;
         if (student.house === 'Gryffindor') {
             domString += `<img class="card-img-top" src="https://images.pottermore.com/bxd3o8b291gf/49zkCzoZlekCmSq6OsycAm/da6278c1af372f18f8b6a71b226e0814/PM_House_Pages_400_x_400_px_FINAL_CREST2.png?w=550&h=550&fit=thumb&f=center&q=85">`;
         } else if (student.house === 'Hufflepuff') {
@@ -24,40 +28,39 @@ const domStringBuilder = () => {
         } else if (student.house === 'Slytherin') {
             domString += `<img class="card-img-top" src="https://images.pottermore.com/bxd3o8b291gf/4U98maPA5aEUWcO8uOisOq/e01e17cc414b960380acbf8ace1dc1d5/PM_House_Pages_400_x_400_px_FINAL_CREST4.png?w=550&h=550&fit=thumb&f=center&q=85">`;
         }
-        domString += `<button type="button" id="expel" class="btn btn-danger" data-toggle="modal" data-target="#exampleModal">
-        Launch demo modal>Expel</button>`;
+        domString += `  <button type="button" id="${student.name}" class="btn expel btn-danger">Expel</button>`;
         domString += `</div>`;
     }); // end forEach
+    console.log(domString);
     printToDom('studentCards', domString);
-    document.getElementById('expel').addEventListener('click', expelStudent);
+    expelEvent();
 };
 
-const expelStudent = () => {
-    alert("underage magic is prohibited and punished by expulsion");
-    let domString = '';
-    // modal
-    domString += `<div class="modal" tabindex="-1" role="dialog">`;
-    domString +=    `<div class="modal-dialog" role="document">`;
-    domString += `    <div class="modal-content">`;
-    domString += `      <div class="modal-header">`;
-    domString += `        <h5 class="modal-title">Modal title</h5>`;
-    domString += `        <button type="button" class="close" data-dismiss="modal" aria-label="Close">`;
-    domString += `          <span aria-hidden="true">&times;</span>`;
-    domString += `        </button>`;
-    domString += `      </div>`;
-    domString += `      <div class="modal-body">`;
-    domString += `        <p>Modal body text goes here.</p>`;
-    domString += `      </div>`;
-    domString += `      <div class="modal-footer">`;
-    domString += `        <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>`;
-    domString += `        <button type="button" class="btn btn-primary">Save changes</button>`;
-    domString += `      </div>`;
-    domString += `    </div>`;
-    domString += `  </div>`;
-    domString += `</div>`;
-  
+// const expelStudent = () => {
+//     let domString = '';
+//     // modal
+//     domString += `<div id="myDialog" class="modal" tabindex="-1" role="dialog">`;
+//     domString +=    `<div class="modal-dialog" role="document">`;
+//     domString += `    <div class="modal-content">`;
+//     domString += `      <div class="modal-header">`;
+//     domString += `        <h5 class="modal-title">Modal title</h5>`;
+//     domString += `        <button type="button" class="close" data-dismiss="modal" aria-label="Close">`;
+//     domString += `          <span aria-hidden="true">&times;</span>`;
+//     domString += `        </button>`;
+//     domString += `      </div>`;
+//     domString += `      <div class="modal-body">`;
+//     domString += `        <p>Modal body text goes here.</p>`;
+//     domString += `      </div>`;
+//     domString += `      <div class="modal-footer">`;
+//     domString += `        <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>`;
+//     domString += `        <button type="button" class="btn btn-primary">Save changes</button>`;
+//     domString += `      </div>`;
+//     domString += `    </div>`;
+//     domString += `  </div>`;
+//     domString += `</div>`;
 
-};
+//     // document.getElementById("myDialog").showModal(domString);
+// };
 
 const studentAdd = () => {
     let newStudent = {
@@ -70,7 +73,8 @@ const studentAdd = () => {
 
 
 const selectHouse = () => {
-    const hogSort = Math.ceil(Math.random() * 4);
+    const hogSort = (Math.floor(Math.random() * 4) +1);
+    console.log(hogSort);
     if (hogSort === 1) {
         return 'Gryffindor';
     } else if (hogSort === 2) {
@@ -80,6 +84,29 @@ const selectHouse = () => {
     } else if (hogSort === 4) {
         return 'Slytherin';
     }
+};
+
+
+const removeStudent = (e) => {
+    console.log(e.currentTarget.id);
+    students.forEach((object, i) => {
+        if (object.name === e.currentTarget.id) {
+            console.log(students);
+            students.splice(i, 1);
+            console.log(students);
+        }
+    })
+    domStringBuilder();
+};
+
+
+
+const expelEvent = () => {
+    let everyCard = document.querySelectorAll('.expel');
+    console.log(everyCard);
+    everyCard.forEach((card) => {
+    card.addEventListener('click', removeStudent);
+    });
 };
 
 
@@ -93,13 +120,24 @@ const domFormBuilder = () => {
         domString += `      <button type="submit" id="sortMe" class="btn btn-primary">Let's Get Sorted!</button>`;
         domString += `</form>`;
     
-    printToDom('sortingForm', domString);
-    document.getElementById('sortMe').addEventListener('click', studentAdd);
+        
+        printToDom('sortingForm', domString);
+        secondEvent();
+    // let x = document.getElementsByClassName('expel');
+    // console.log(x.length);
+    // for (let i = 0; i < x.length; i++) {
+    //     console.log(x[i]);
+    // }
+
+ 
 };
 
 
 const buttonEvents = () => {
     document.getElementById('letsGetSorting').addEventListener('click', domFormBuilder);
+};
+const secondEvent = () => {
+    document.getElementById('sortMe').addEventListener('click', studentAdd);
 };
 
 
